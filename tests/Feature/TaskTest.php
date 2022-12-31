c81abe213b9339e972af1a2d81b8edb21b6a758f
miisan-miisan <77946692+miisan-miisan@users.noreply.github.com>
2022-12-31 17:05:58 +0900

    react=router設定とviewの整え、scssの適用

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -13,7 +13,7 @@ class TaskTest extends TestCase
     /**
      * @test
      */
-    public function 一覧を取得()
+    public function 一覧を取得できる()
     {
         $tasks = Task::factory()->count(10)->create();
 
@@ -22,5 +22,82 @@ class TaskTest extends TestCase
         $response
             ->assertOk()
             ->assertJsonCount($tasks->count());
+    }
+
+    /**
+     * @test
+     */
+    public function 登録することができる()
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+        $response = $this->postJson('api/tasks', $data);
+        $response
+            ->assertJsonFragment($data)
+            ->assertCreated();
+    }
+
+    /**
+     * @test
+     */
+    public function 更新することができる()
+    {
+        $task = Task::factory()->create();
+        $task->title = '書き換え';
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function 削除することができる()
+    {
+        $tasks = Task::factory()->count(10)->create();
+        $response = $this->deleteJson("api/tasks/1");
+        $response
+            ->assertOk();
+
+        $response = $this->getJson("api/tasks");
+        $response
+            ->assertJsonCount($tasks->count() - 1);
+    }
+
+    /**
+     * @test
+     */
+    public function タイトルが空の場合は登録できない()
+    {
+        $data = [
+            'title' => ''
+        ];
+        $response = $this->postJson('api/tasks', $data);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは必ず指定してください。'
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function タイトルが255文字の場合は登録できない()
+    {
+        $data = [
+            'title' => str_repeat('a', 255)
+        ];
+        $response = $this->postJson('api/tasks', $data);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは、255文字以下で指定してください。'
+            ]);
     }
 }
